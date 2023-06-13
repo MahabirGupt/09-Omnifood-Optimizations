e852c7bda8960eb1a052d1d0a25b793697d404d3
Mahabir <mahabirg81@gmail.com>
2023-06-13 21:24:13 +0800

    implement current year js for copyright year

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
+console.log("Hello World!");
+
+const myName = "Mahabir Gupta";
+console.log(myName);
+
+const h1 = document.querySelector(".heading-primary");
+console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+
+//   // to manipulate the CSS
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+// Set current year
+const year = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+// function checkFlexGap() {
+//   var flex = document.createElement("div");
+//   flex.style.display = "flex";
+//   flex.style.flexDirection = "column";
+//   flex.style.rowGap = "1px";
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+//   // document.body.appendChild(flex);
+//   var isSupported = flex.scrollHeight === 1;
+//   // flex.parentNode.removeChild(flex);
+//   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
-checkFlexGap();
+//   // if (!isSupported) document.body.classList.add("no-flexbox-gap");
+// }
+// checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
